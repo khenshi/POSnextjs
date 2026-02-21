872cf526b0d2f6cd5e0df6c7a933cd27200cdc0c
Khenyshi Hinlog <hinlogkhenyshi@gmail.com>
2026-02-21 08:18:14 +0800

    chapter 10

---
+++ nextjs-dashboard/app/lib/action.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import { z } from 'zod';
+import postgres from 'postgres'
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formdata: FormData) {
+    const {customerId, amount, status} = CreateInvoice.parse({
+        customerId: formdata.get('customerId'), 
+        amount: formdata.get('amount'),
+        status: formdata.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+} catch (error) {
+  console.error("DB ERROR:", error);
+  throw new Error("Failed to create invoice");
+}
+    // revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
