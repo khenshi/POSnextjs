a48a48ee480e0689407c9ac91f63617ec21b9f68
Khenyshi Hinlog <hinlogkhenyshi@gmail.com>
2026-02-22 22:06:44 +0800

    test new updated eslint config

--- nextjs-dashboard/eslint.config.mjs
+++ nextjs-dashboard/eslint.config.mjs
@@ -1,8 +1,15 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
+  ...compat.extends('next/core-web-vitals'),
   globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
